407ffbb565cb689c8836b12d29b2a7dcdbd0303d
Serg <ksyssau@ya.ru>
2023-07-17 23:55:58 +0300

    1

---
+++ src/main/java/pro/sky/collections/skypro_collections/SkyproCollectionsApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.collections.skypro_collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCollectionsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SkyproCollectionsApplication.class, args);
+	}
+
+}
